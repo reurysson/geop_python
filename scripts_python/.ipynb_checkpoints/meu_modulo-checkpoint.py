878da55b899cd853ccdaaa31265e8082a77ce72e
Reurysson <reurysson@ifpi.edu.br>
2025-04-02 16:49:22 -0300

    Atualizações
    Atualizações com correções ortográficas

--- scripts_python/.ipynb_checkpoints/meu_modulo-checkpoint.py
+++ scripts_python/.ipynb_checkpoints/meu_modulo-checkpoint.py
@@ -5,11 +5,15 @@ disciplina = "Introdução à Linguagem Python para Análise de Dados"
 periodo = "III"
 
 def saudacao(nome):
-    print(f"Seja bem bindo a disciplina de Python, {nome}!")
+    print(f"Seja bem vindo à disciplina de Python, {nome}!")
 
 #_____________________________________________________________
 def soma(n1, n2):
     return n1 + n2
+#_____________________________________________________________
+
+def area_circulo(r):
+    return 3.14159 * r **2
 
 #_____________________________________________________________
 import math
