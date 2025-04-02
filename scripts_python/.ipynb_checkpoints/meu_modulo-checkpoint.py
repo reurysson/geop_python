30fb9eab1ca6566fba48a62d9dc0bd301e938a96
Reurysson <reurysson@ifpi.edu.br>
2025-04-01 22:27:53 -0300

    aula 03 iniciada

---
+++ scripts_python/.ipynb_checkpoints/meu_modulo-checkpoint.py
@@ -0,0 +1,20 @@
+curso = "Tecnologia em Geoprocessamento"
+
+disciplina = "Introdução à Linguagem Python para Análise de Dados"
+
+periodo = "III"
+
+def saudacao(nome):
+    print(f"Seja bem bindo a disciplina de Python, {nome}!")
+
+#_____________________________________________________________
+def soma(n1, n2):
+    return n1 + n2
+
+#_____________________________________________________________
+import math
+
+def hipotenusa(a, b):
+    return math.sqrt(a**2 + b**2)
+
+#_____________________________________________________________
